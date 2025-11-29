7073249e66d7198028f5b588be95e62a54f0bec7
int-arsh <jainaakash303@gmail.com>
2025-11-29 21:39:57 +0530

    Add Entry model and update admin registration

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -12,5 +12,15 @@ class Topic(models.Model):
         return self.text
     
 
-class Entry(models.model):
+class Entry(models.Model):
+    """Something specific learned about the topic"""
+    topic=models.ForeignKey(Topic,on_delete=models.CASCADE)
+    text=models.TextField()
+    date_added=models.DateTimeField(auto_now_add=True)
     
+    class Meta:
+        verbose_name_plural = 'entries'
+        
+    def __str__(self):
+        """ Return the string representation of the model"""
+        return f"{self.text[:50]}..."
