3adf4bcd88c9bf175739ce4724ba62301dfd6ace
int-arsh <jainaakash303@gmail.com>
2025-12-08 19:30:50 +0530

    Add user registration functionality with form handling and template

--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+def register(request):
+    """Regsiter a new user"""
+    # get request give blank form
+    if request.method != 'POST':
+        form = UserCreationForm()
+    
+    # process the form if submitted
+    else:
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return redirect('learning_logs:index')
+    
+    # Display an blank or invalid form
+    context= {'form': form }
+    return render(request, 'registration/register.html', context)
+        
+    
+    
+    
+        
+    
