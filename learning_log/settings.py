00ee844276ecfd2e7e904b659bf2d5f6a44bd32a
int-arsh <jainaakash303@gmail.com>
2025-12-10 18:01:00 +0530

    Enhance user experience in registration templates and update dependencies
    - Updated the logged_out.html template to use a header for better visibility.
    - Modified the login.html template to include Bootstrap styling and improved error handling.
    - Added new dependencies including attrs, beautifulsoup4, click, colorama, curlylint, dj-database-url, django-bootstrap4, pars, pathspec, soupsieve, toml, typing-extensions, tzdata, and whitenoise in the uv.lock file.
    - Ensured compatibility with Django 5.2.8 and updated the learning-log package requirements accordingly.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'learning_logs',
     'users',
     
+    # third party apps
+    'bootstrap4',
+    
     # default apps.
     'django.contrib.admin',
     'django.contrib.auth',
@@ -48,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -130,3 +136,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # my settings
 LOGIN_URL = 'users:login'
+
